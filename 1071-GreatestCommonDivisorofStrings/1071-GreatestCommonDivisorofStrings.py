93f19af8856f51208c2108fe749caa93b8698d47
Youngkoorosh <koorosh.nrp@gmail.com>
2025-07-02 02:46:45 +0330

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -0,0 +1,12 @@
+# Last updated: 7/2/2025, 2:46:44 AM
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        ic = ""
+        # Check if concatenations are equal in both orders
+        if str1 + str2 == str2 + str1:
+            # Find the GCD of lengths
+            max_len = gcd(len(str1), len(str2))
+            ic = str1[:max_len]
+                
+        return ic
+        
